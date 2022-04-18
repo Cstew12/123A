841443429ce84db2945c124480457ea48caefbe5
Cstew12 <92701896+Cstew12@users.noreply.github.com>
2022-04-18 14:55:20 -0700

    not trash

---
+++ SocialDistancers/ios/Pods/Target Support Files/Pods-SocialDistancers-SocialDistancersTests/Pods-SocialDistancers-SocialDistancersTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SocialDistancers_SocialDistancersTests : NSObject
+@end
+@implementation PodsDummy_Pods_SocialDistancers_SocialDistancersTests
+@end
