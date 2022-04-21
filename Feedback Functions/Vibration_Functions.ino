47b2277fff06455aa67217ed4a82ad22e597c1e7
terrenceng88 <terrenceng321@gmail.com>
2022-04-20 18:52:49 -0700

    filled in trigger Haptic logic

--- Feedback Functions/Vibration_Functions.ino
+++ Feedback Functions/Vibration_Functions.ino
@@ -16,39 +16,40 @@
 int current_level = 0; //current vibration level
 int last_level = LEVEL_5; //last known non zero vibration level
 
+/*
 void setup() {
   pinMode(MOTOR, OUTPUT); //setup vibration motor pins
 }
+*/
 
 /*
 void loop() {
   analogWrite(MOTOR, data); //vibrate MOTOR at specified data level
 */
 
-//vibrates the vibration motors at the specified level when not at a safe distance (< 6ft)
-//could remove level argument and just run the motor at the current level global variable
-void runVibrate(bool safe, int level) {
-  if(safe) { //more than 6 feet away
-    analogWrite(MOTOR, 0); //vibrate MOTOR at specified data level
-  } else { //less than 6 feet away
-    analogWrite(MOTOR, level); //vibrate MOTOR at specified data level
-  }
+//vibrates the vibration motors at the specified current level (global variable)
+void triggerHaptic() {
+  current_level = last_level; //set vibration to last level
+  analogWrite(MOTOR, current_level); //vibrate MOTOR at specified data level
+}
+
+//initiallization code for vibrations
+void initialize_vibration() {
+  pinMode(MOTOR, OUTPUT); //setup vibration motor pins
 }
 
 //turn off vibration 
+//save last vibration level
 void vibrationOFF() {
    last_level = current_level; //save current vibration level
    current_level = OFF; //turn off 
+   analogWrite(MOTOR, OFF); //dont send any power to the vibration motor
 }
 
-//turn on vibratrion (level is same as last known vibration, LEVEL_5 otherwise)
-void vibrationON() {
-  if(last_level == 0) { //if previously no vibration before it was off
-    current_level = LEVEL_5; //set vibraiton to max
-  } else {
-    current_level = last_level; //set vibration to last level
-  }
-}
+//turn on vibratrion (level is same as last known vibration)
+//void vibrationON() {
+//   current_level = last_level; //set vibration to last level
+//}
 
 //change vibration amount to level
 //should be one of the predefined vibration levels
