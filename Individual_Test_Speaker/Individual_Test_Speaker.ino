89d52541a3834ffe76e8c460030d80662dc1be2a
mvoorhes <100326501+mvoorhes@users.noreply.github.com>
2022-04-28 09:52:34 -0700

    Update: Asynchronous now

--- Individual_Test_Speaker/Individual_Test_Speaker.ino
+++ Individual_Test_Speaker/Individual_Test_Speaker.ino
@@ -1,19 +1,34 @@
 #include "pitches.h"
 
-int echo = 6;
-int trig = 7;
+#define SOUND_PIN 5
 
-int melody[] = {NOTE_D4, NOTE_D4, NOTE_D5, NOTE_A4, NOTE_GS4, NOTE_G4, NOTE_F4, NOTE_D4, NOTE_F4, NOTE_G4};
-int note_durations[] = {4, 4, 8, 12, 8, 8, 8, 4, 4, 4};
+int melody_1[] = {NOTE_D4, NOTE_D4, NOTE_D5, NOTE_A4, NOTE_GS4, NOTE_G4, NOTE_F4, NOTE_D4, NOTE_F4, NOTE_G4, END};
+int note_durations_1[] = {4, 4, 8, 12, 8, 8, 8, 4, 4, 4};
 
-int speed = 5;
+int melody_2[] = {NOTE_A4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_B4, NOTE_G4, NOTE_A4, END};
+int note_durations_2[] = {4, 4, 4, 4, 8, 4, 12};
+
+int melody_3[] = {NOTE_C8, NOTE_CS2};
+int note_durations_3[] = {4, 4};
+
+int speed = 20;
+int index = 0;
+int length = 2;
+
 void loop_song() {
-  for (int i = 0; melody[i] != -1; i++) {
-    int noteDuration = speed * note_durations[i];
-    tone(3, melody[i], noteDuration * .95);
-    delay(noteDuration);
-    noTone(3);
+
+  if (index >= length) {
+    index = 0;
   }
+  int noteDuration = speed * note_durations_3[index];
+  if (noteDuration > 100) {
+    index++;
+    return;
+  }
+  tone(SOUND_PIN, melody_3[index], noteDuration * .95);
+  delay(noteDuration);
+  noTone(SOUND_PIN);
+  index++;
 }
 
 void setup() {
